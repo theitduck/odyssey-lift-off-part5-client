34c1af6a85af6f5977eebd9e8d94f9233951cf80
Mory DOSSO <mory.dosso_ext@sonepar.com>
2022-07-05 16:17:48 +0200

    fix(main): use length instead of durationInSecond

--- src/components/track-detail.js
+++ src/components/track-detail.js
@@ -24,7 +24,7 @@ const TrackDetail = ({ track }) => {
     description,
     thumbnail,
     author,
-    durationInSeconds,
+    length,
     modulesCount,
     modules,
     numberOfViews,
@@ -50,7 +50,7 @@ const TrackDetail = ({ track }) => {
             </IconAndLabel>
             <IconAndLabel>
               <IconTime width="14px" />
-              <div>{humanReadableTimeFromSeconds(durationInSeconds)}</div>
+              <div>{humanReadableTimeFromSeconds(length)}</div>
             </IconAndLabel>
           </DetailItem>
           <DetailItem>
@@ -78,7 +78,7 @@ const TrackDetail = ({ track }) => {
                 <li key={module.title}>
                   <div>{module.title}</div>
                   <ModuleLength>
-                    {humanReadableTimeFromSeconds(module.durationInSeconds)}
+                    {humanReadableTimeFromSeconds(module.length)}
                   </ModuleLength>
                 </li>
               ))}
